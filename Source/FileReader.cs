c9bd6ddd68c81ad60fe55987e2d79d79c90116b0
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-10-17 22:12:17 -0300

    files reorganized

---
+++ Source/FileReader.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using System;
+
+namespace Source
+{
+    class FileReader
+    {
+        public string[] readFile(string file)
+        {
+            string[] numberValues = { };
+
+            if (File.Exists(file))
+            {
+                numberValues = File.ReadAllLines(file);
+            }
+
+            return numberValues;
+        }
+
+        public string combinePaths(string fileToCombine)
+        {
+            var currentDir = Environment.CurrentDirectory;
+            var fileCombined = Path.GetFullPath(Path.Combine(currentDir, "files", fileToCombine));
+            return fileCombined;
+        }
+    }
+}
---
+++ Source/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Source
+{
+    class Program
+    {
+        private static readonly string file1 = "numbers-1.txt";
+        private static readonly string file2 = "numbers-2.txt";
+        static void Main(string[] args)
+        {
+            var fileReader = new FileReader();
+            var firstFile = fileReader.combinePaths(file1);
+            var secondFile = fileReader.combinePaths(file2);
+
+            var firstList = fileReader.readFile(firstFile);
+            
+        }
+
+        
+    }
+}
