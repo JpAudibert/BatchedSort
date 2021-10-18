aa9fecb366759de76c220a8a77c51963e3ef6be4
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-10-17 21:22:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BatchedSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
