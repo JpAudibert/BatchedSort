933fec180ed2afa6ffde9fccf22265e878b6806c
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-10-17 22:04:54 -0300

    File Reader implemented

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
 
 namespace BatchedSort
 {
     class Program
     {
+        private static readonly string file1 = "numbers-1.txt";
+        private static readonly string file2 = "numbers-2.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileReader = new FileReader();
+            var firstFile = fileReader.combinePaths(file1);
+            var secondFile = fileReader.combinePaths(file2);
+
+            fileReader.readFile(firstFile);
         }
+
+        
     }
 }
